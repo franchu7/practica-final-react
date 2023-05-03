f59341e65a25e6d01defbca046e299355d862de1
franchu7 <73340817+franchu7@users.noreply.github.com>
2023-05-03 17:55:41 +0200

    03-05-2023

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BackgroundImage, Grid, AppShell, Title, Paper, Button, Stack, Group, Image } from '@mantine/core'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BackgroundImage h='100%' w='100%' src='/background.jpg'>
+      <AppShell padding='xl'>
+        <Grid>
+          <Grid.Col md={6}>
+            <Stack spacing={'sm'}>
+              <Paper p={'md'}>
+                <Title order={1} fw={500} align='center'>
+                  Categorías
+                </Title>
+              </Paper>
+              <Button color='green'>Añadir categoría</Button>
+              <Paper bg={'gray'} p='md'>
+                <Paper bg={'cyan'} p='sm'>
+                  <Group position='center' spacing={'xl'}>
+                    <Title order={2} fw={500}>
+                      Juegos de terror
+                    </Title>
+                    <Button color='green'>Añadir juego</Button>
+                  </Group>
+                  <Grid>
+                    <Grid.Col md={6}>
+                      <Image src={'/resident_evil_2.jpg'} />
+                    </Grid.Col>
+                  </Grid>
+                </Paper>
+              </Paper>
+            </Stack>
+          </Grid.Col>
+          <Grid.Col md={6}>
+            <Paper p={'md'}>
+              <Title order={1} fw={500} align='center'>
+                Cesta de la compra
+              </Title>
+            </Paper>
+          </Grid.Col>
+        </Grid>
+      </AppShell>
+    </BackgroundImage>
+  )
 }
 
-export default App;
+export default App
